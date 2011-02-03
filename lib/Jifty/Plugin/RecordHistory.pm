d8d9443a8fe9255dd266216dc46e37716c960f48
Shawn M Moore <sartak@bestpractical.com>
2011-02-02 19:25:03 -0500

    Layout

---
+++ lib/Jifty/Plugin/RecordHistory.pm
@@ -0,0 +1,33 @@
+package Jifty::Plugin::RecordHistory;
+use strict;
+use warnings;
+use base qw/Jifty::Plugin/;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Jifty::Plugin::RecordHistory - track all changes made to a record class
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<Jifty::Plugin::ActorMetadata>
+
+=head1 AUTHOR
+
+Shawn M Moore C<< <sartak@bestpractical.com> >>
+
+=head1 LICENSE
+
+Jifty::Plugin::RecordHistory is Copyright 2011 Best Practical Solutions, LLC.
+Jifty::Plugin::RecordHistory is distributed under the same terms as Perl itself.
+
+=cut
+
