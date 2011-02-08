244ca3e9d9fa7acabb79c5bf448e2af8fd6c6b4d
Shawn M Moore <sartak@bestpractical.com>
2011-02-08 00:01:44 -0500

    Fields for TransactionEntry

--- lib/Jifty/Plugin/RecordHistory/Model/TransactionEntry.pm
+++ lib/Jifty/Plugin/RecordHistory/Model/TransactionEntry.pm
@@ -6,6 +6,22 @@ use Jifty::DBI::Schema;
 use Jifty::Record schema {
     column transaction =>
         refers to Jifty::Plugin::RecordHistory::Model::Transaction,
+        label is 'Transaction',
+        is immutable;
+
+    column field =>
+        type is 'varchar',
+        label is 'Field',
+        is immutable;
+
+    column old_value =>
+        type is 'text',
+        label is 'Old value',
+        is immutable;
+
+    column new_value =>
+        type is 'text',
+        label is 'New value',
         is immutable;
 };
 
@@ -20,5 +36,4 @@ sub delegate_current_user_can {
 }
 
 1;
-
 
