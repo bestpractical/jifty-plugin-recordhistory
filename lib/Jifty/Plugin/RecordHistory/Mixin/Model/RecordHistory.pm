03a1b758bbf60e8974b3658c388f177713a7684a
Shawn M Moore <sartak@bestpractical.com>
2011-02-09 00:13:53 -0500

    Export a transactions method from the mixin

--- lib/Jifty/Plugin/RecordHistory/Mixin/Model/RecordHistory.pm
+++ lib/Jifty/Plugin/RecordHistory/Mixin/Model/RecordHistory.pm
@@ -1,10 +1,15 @@
 package Jifty::Plugin::RecordHistory::Mixin::Model::RecordHistory;
 use strict;
 use warnings;
+use base 'Exporter';
+
+our @EXPORT = qw(transactions);
 
 sub import {
     my $class = shift;
     my $caller = caller;
+
+    $class->export_to_level(1, @_);
 
     $caller->add_trigger(after_set => sub {
         my $record = shift;
@@ -16,6 +21,22 @@ sub import {
 
         # add to the current transaction
     });
+}
+
+sub transactions {
+    my $self = shift;
+    my $transactions = Jifty::Plugin::RecordHistory::Model::TransactionCollection->new;
+    $transactions->limit(
+        column => 'record_class',
+        value  => ref($self),
+    );
+
+    $transactions->limit(
+        column => 'record_id',
+        value  => $self->id,
+    );
+
+    return $transactions;
 }
 
 1;
