9f4da81426585809aeae7f6b5a1323da1f7a29bc
Shawn M Moore <sartak@bestpractical.com>
2011-02-09 23:09:57 -0500

    Doc for views

--- lib/Jifty/Plugin/RecordHistory.pm
+++ lib/Jifty/Plugin/RecordHistory.pm
@@ -38,11 +38,28 @@ and using the mixin to your record class(es) to enjoy transaction history. The
 mixin even hooks into Jifty itself to observe record creation, updates, and
 deletions.
 
+=head2 Grouping
+
 By default, the only mechanism that groups together change_fields onto a single
 change object is L<Jifty::Action::Record::Update> (and its subclasses that do
 not override C<take_action>). But if you want to make a number of field updates
 that need to be grouped into a single logical change, you can call
 C<start_change> and C<end_change> yourself on the record object.
+
+=head2 Views
+
+If you want to display changes for a record class, mount the following into
+your view tree to expose a default view at C</foo/history?id=42> (or you can of
+course set C<id> via dispatcher rule).
+
+    use Jifty::Plugin::RecordHistory::View;
+    alias Jifty::Plugin::RecordHistory::View under '/foo/history', {
+        object_type => 'Foo',
+    };
+
+Alternatively, if you want to extend the default templates, you can subclass
+L<Jifty::Plugin::RecordHistory::View> in the same way as
+L<Jifty::View::Declare::CRUD>.
 
 =head1 SEE ALSO
 
