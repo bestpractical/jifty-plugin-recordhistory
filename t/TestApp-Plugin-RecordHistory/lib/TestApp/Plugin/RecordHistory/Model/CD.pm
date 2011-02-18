b4025a281f9eb30713e18ecc87c8de651c1e11f9
Shawn M Moore <sartak@bestpractical.com>
2011-02-18 16:49:45 -0500

    Test for cascaded_delete => 0

---
+++ t/TestApp-Plugin-RecordHistory/lib/TestApp/Plugin/RecordHistory/Model/CD.pm
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+
+package TestApp::Plugin::RecordHistory::Model::CD;
+use Jifty::DBI::Schema;
+
+use TestApp::Plugin::RecordHistory::Record schema {
+    column title =>
+        type is 'varchar';
+    column artist =>
+        type is 'varchar';
+};
+
+use Jifty::Plugin::RecordHistory::Mixin::Model::RecordHistory (
+    cascaded_delete => 0,
+);
+
+1;
+
